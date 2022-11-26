c8e2c7a9133e25686e13ff8db8b075c598677304
yash <ysuthar8055@gmail.com>
2022-11-26 10:08:23 +0530

    now image wil be changed

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 function calc(){
 amount=document.getElementById('kg').value
-temp=document.getElementById('option').value;
-let answer=amount*temp
+let temp=document.getElementById('option').value;
+let answer=(amount*temp).toFixed(2)
 document.getElementById("inp").innerHTML=answer
+let img=document.getElementById('img');
+switch(temp){
+    case "0.91":
+        console.log(temp)
+        img.src="images/venus.png"
+        break
+    case "0.38":
+        img.src="images/mars.png"
+        break
+    case "0.16":
+        img.src="images/moon.png"
+        break
+    case "1":
+        img.src="images/earth.png"
+        break
+    case "0.38":
+        img.src="images/mercury.png"
+        break
+    case "2.34":
+        img.src="images/jupiter.png"
+        break
+    case "1.06":
+        img.src="images/saturn.png"
+        break
+    case "0.92":
+        img.src="images/uranus.png"
+        break
+    case "1.19":
+        img.src="images/neptune.png"
+        break; 
+    case "0.91":
+        img.src="images/pluto.png"
+        break;           
+}
 }
